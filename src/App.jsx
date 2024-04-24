46f7e43c49931a068159543ebdd0927e59638a89
Boaz Onyango <boazbathenal123@gmail.com>
2024-04-24 09:39:08 +0300

    modified appointment form , router and Layout components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Router from "./components/Router";
 function App() {
   return (
@@ -8,18 +7,3 @@ function App() {
   );
 }
 export default App;
-=======
-import Router from "./components/Router"
-function App() {
-
-  return (
-   <>
-   <Router/>
-   </>
-  )
-}
-
-export default App
-
-  
->>>>>>> 8f4e99dca015c93fb6cbdbc082c4e8e34bc7c016
